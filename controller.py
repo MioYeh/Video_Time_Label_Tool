d2577203e543cdfe313052e95bf8573b0e069a63
MioYeh <mio861225@gmail.com>
2025-03-27 18:35:26 +0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -10,7 +10,6 @@ class VideoController:
 
     def handle_key(self, key):
         current_time = self.view.cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
-
         formatted_time = time.strftime('%M:%S', time.gmtime(current_time))
         if not self.recording:
             self.start_time = formatted_time
@@ -19,8 +18,18 @@ class VideoController:
             record_text = f"{self.start_time} - {formatted_time}"
             if key == 'C':
                 self.view.record_c_list.addItem(QListWidgetItem(record_text))
+                self.view.record_c_label.setText(f"Time: ")
             elif key == 'W':
                 self.view.record_w_list.addItem(QListWidgetItem(record_text))
+                self.view.record_w_label.setText(f"Time: ")
             elif key == 'R':
                 self.view.record_r_list.addItem(QListWidgetItem(record_text))
+                self.view.record_r_label.setText(f"Time: ")
+                
+            # elif key == 'R':
+            #     self.view.record_r_list.addItem(QListWidgetItem(record_text))
+            #     self.view.record_r_label.setText(f"Time: ")
+            # elif key == 'R':
+            #     self.view.record_r_list.addItem(QListWidgetItem(record_text))
+            #     self.view.record_r_label.setText(f"Time: ")
             self.recording = False
