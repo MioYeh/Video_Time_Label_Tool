78a5fcd058a91afef43a3869ac663719d2d04142
MioYeh <mio861225@gmail.com>
2025-04-17 10:38:29 +0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -8,9 +8,16 @@ class VideoController:
         self.recording = False
         self.start_time = None
 
+    def formatTime(self, seconds):
+        hours = int(seconds // 3600)
+        minutes = int((seconds % 3600) // 60)
+        secs = int(seconds % 60)
+        millis = int((seconds - int(seconds)) * 1000)
+        return f"{hours:02}:{minutes:02}:{secs:02}.{millis:03}"
+    
     def handle_key(self, key):
         current_time = self.view.cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
-        formatted_time = time.strftime('%M:%S', time.gmtime(current_time))
+        formatted_time = self.formatTime(round(current_time, 3))
         if not self.recording:
             self.start_time = formatted_time
             self.recording = True
